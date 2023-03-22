05df8e0250a0685b7fad138ef136720db6302a85
Igor Barroso Almeida <igorba867@gmail.com>
2023-03-22 20:20:21 -0300

    organização em pastas

---
+++ src/Recebedor.java
@@ -0,0 +1,38 @@
+package src;
+
+/* Importações */
+import java.io.InputStream;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Virginia
+ * @grupo Igor Barroso Almeida, Leandro Rodrigues Marques, João Pedro Neffa,
+ *        Eduardo Evangelista Santos
+ */
+public class Recebedor implements Runnable {
+    // Atributos
+    private InputStream servidor;
+    private String name;
+
+    public Recebedor(InputStream servidor, String name) {
+        this.servidor = servidor;
+        this.name = name;
+    }
+
+    @Override
+    public void run() {
+        // recebe msgs do servidor e imprime na tela
+        Scanner s = new Scanner(this.servidor);
+
+        while (s.hasNextLine()) {
+            String msg = s.nextLine();
+            // Pega o nome do usuario (primeira posição do texto separado por espaço)
+            String nome = msg.split(" ")[0];
+
+            if (!(nome.equals(this.name))) {
+                System.out.println(msg);
+            }
+        }
+    }
+}
---
+++ src/TrataCliente.java
@@ -0,0 +1,32 @@
+package src;
+
+/* Importações */
+import java.io.InputStream;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Virginia
+ * @grupo Igor Barroso Almeida, Leandro Rodrigues Marques, João Pedro Neffa,
+ *        Eduardo Evangelista Santos
+ */
+public class TrataCliente implements Runnable {
+
+  private InputStream cliente;
+  private Servidor servidor;
+
+  public TrataCliente(InputStream cliente, Servidor servidor) {
+    this.cliente = cliente;
+    this.servidor = servidor;
+  }
+
+  @Override
+  public void run() {
+    // quando chegar uma msg, distribui pra todos
+    Scanner s = new Scanner(this.cliente);
+    while (s.hasNextLine()) {
+      servidor.distribuiMensagem(s.nextLine());
+    }
+    s.close();
+  }
+}
