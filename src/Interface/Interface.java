ce23da5b21156681a0fa1d332428a633e33eae7f
Igor Barroso Almeida <igorba867@gmail.com>
2023-03-28 09:14:53 -0300

    Começo interface

---
+++ src/Interface/Interface.java
@@ -0,0 +1,23 @@
+package src.Interface;
+
+/* Imports */
+import java.awt.GridBagLayout;
+import javax.swing.*;
+import java.awt.*;
+
+public class Interface {
+    /* Atributos */
+    private static JFrame mainWindow = new JFrame("Chat");
+    private static JTextArea InputArea = new JTextArea();
+    
+
+    public static void main(String[] args){
+        
+
+
+        mainWindow.setVisible(true);
+    }
+
+
+    
+}
