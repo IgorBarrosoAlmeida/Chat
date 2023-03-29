843f1aad05e3d7353befc4da1a4a2b9825b10593
leandro-rodrigueds <120420969+leandro-rodrigueds@users.noreply.github.com>
2023-03-29 17:45:30 -0300

    Update Interface

--- src/Interface/Interface.java
+++ src/Interface/Interface.java
@@ -2,20 +2,66 @@ package src.Interface;
 
 /* Imports */
 import java.awt.GridBagLayout;
+import java.awt.event.*;
 import javax.swing.*;
 import java.awt.*;
 
-public class Interface {
+public class Interface extends JFrame implements KeyListener, ActionListener{
     /* Atributos */
     private static JFrame mainWindow = new JFrame("Chat");
-    private static JTextArea InputArea = new JTextArea();
+    private static JPanel writeArea = new JPanel();
+    private static JTextField input = new JTextField(20);
+    private static JButton sendButton = new JButton("Send");
     
-
     public static void main(String[] args){
         
+        mainWindow.setSize(new Dimension(400,800));
+
+        // Painel
+        writeArea.setSize(new Dimension(400,800));
+        input.setSize(new Dimension(100, 300));
+        input.setBackground(Color.WHITE);
+        // font collor
+        input.setForeground(Color.DARK_GRAY);
+        writeArea.setBackground(Color.GRAY);
+        sendButton.setLocation(300,300);
 
 
+        writeArea.add(input, BorderLayout.WEST);
+        writeArea.add(sendButton, BorderLayout.EAST);
+        mainWindow.add(writeArea, BorderLayout.SOUTH);
+
+        input.setVisible(true);
+        writeArea.setVisible(true);
         mainWindow.setVisible(true);
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        //throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
+    }
+
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        // TODO Auto-generated method stub
+        //throw new UnsupportedOperationException("Unimplemented method 'keyPressed'");
+    }
+
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        // TODO Auto-generated method stub
+        //throw new UnsupportedOperationException("Unimplemented method 'keyReleased'");
+    }
+
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        // TODO Auto-generated method stub
+        //throw new UnsupportedOperationException("Unimplemented method 'keyTyped'");
     }
 
 
--- src/logica/TrataCliente.java
+++ src/logica/TrataCliente.java
@@ -1,4 +1,4 @@
-package src;
+package src.logica;
 
 /* Importações */
 import java.io.InputStream;
