6fb9aff5ff29426cba5572ebea5282accc109de9
leandro-rodrigueds <120420969+leandro-rodrigueds@users.noreply.github.com>
2023-04-02 10:39:15 -0300

    Implementando login

--- src/Interface/login.java
+++ src/Interface/login.java
@@ -1,1 +1,14 @@
 package src.Interface;
+
+import javax.swing.JOptionPane;
+
+public class Login {
+
+	public static void main(String[] args) {
+
+		String nomeUsuario = JOptionPane.showInputDialog("Digite seu nome: ");
+		
+		JOptionPane.showMessageDialog(null, "Nome Usuario: "+nomeUsuario,"Nome eh:",JOptionPane.INFORMATION_MESSAGE);
+
+	}
+}
