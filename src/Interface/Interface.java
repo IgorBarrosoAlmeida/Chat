e4cd02c01398804618d5a9c74302394f1b61d72a
Igor Barroso Almeida <igorba867@gmail.com>
2023-04-02 14:29:56 -0300

    Inclui a janela de login

--- src/Interface/Interface.java
+++ src/Interface/Interface.java
@@ -6,26 +6,25 @@ import java.awt.event.*;
 import javax.swing.*;
 import java.awt.*;
 
-public class Interface extends JFrame implements KeyListener, ActionListener{
+public class Interface extends JFrame implements KeyListener, ActionListener {
     /* Atributos */
     private static JFrame mainWindow = new JFrame("Chat");
     private static JPanel writeArea = new JPanel();
     private static JTextField input = new JTextField(20);
     private static JButton sendButton = new JButton("Send");
-    
-    public static void main(String[] args){
-        
-        mainWindow.setSize(new Dimension(400,800));
+
+    public static void main(String[] args) {
+
+        mainWindow.setSize(new Dimension(400, 800));
 
         // Painel
-        writeArea.setSize(new Dimension(400,800));
+        writeArea.setSize(new Dimension(400, 800));
         input.setSize(new Dimension(100, 300));
         input.setBackground(Color.WHITE);
         // font collor
         input.setForeground(Color.DARK_GRAY);
         writeArea.setBackground(Color.GRAY);
-        sendButton.setLocation(300,300);
-
+        sendButton.setLocation(300, 300);
 
         writeArea.add(input, BorderLayout.WEST);
         writeArea.add(sendButton, BorderLayout.EAST);
@@ -36,34 +35,30 @@ public class Interface extends JFrame implements KeyListener, ActionListener{
         mainWindow.setVisible(true);
     }
 
-
     @Override
     public void actionPerformed(ActionEvent e) {
         // TODO Auto-generated method stub
-        //throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
+        // throw new UnsupportedOperationException("Unimplemented method
+        // 'actionPerformed'");
     }
-
 
     @Override
     public void keyPressed(KeyEvent e) {
         // TODO Auto-generated method stub
-        //throw new UnsupportedOperationException("Unimplemented method 'keyPressed'");
+        // throw new UnsupportedOperationException("Unimplemented method 'keyPressed'");
     }
-
 
     @Override
     public void keyReleased(KeyEvent e) {
         // TODO Auto-generated method stub
-        //throw new UnsupportedOperationException("Unimplemented method 'keyReleased'");
+        // throw new UnsupportedOperationException("Unimplemented method
+        // 'keyReleased'");
     }
-
 
     @Override
     public void keyTyped(KeyEvent e) {
         // TODO Auto-generated method stub
-        //throw new UnsupportedOperationException("Unimplemented method 'keyTyped'");
+        // throw new UnsupportedOperationException("Unimplemented method 'keyTyped'");
     }
 
-
-    
 }
--- src/Interface/login.java
+++ src/Interface/login.java
@@ -1,14 +1,56 @@
 package src.Interface;
 
-import javax.swing.JOptionPane;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
-public class Login {
+public class Login extends JFrame implements ActionListener {
+	/* Atributos */
+	JTextField usernameInput;
+	JLabel usernameLabel;
+	JButton loginButton;
+
+	// Construtor
+	Login() {
+		// Config basica
+		this.setSize(new Dimension(400, 200));
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setTitle("Login");
+		this.setLocationRelativeTo(null);
+		this.setLayout(null);
+
+		// Caixa de texto e sua label
+		usernameLabel = new JLabel("Username");
+		usernameLabel.setBounds(160, 10, 200, 50);
+
+		usernameInput = new JTextField();
+		usernameInput.setBounds(50, 50, 280, 30);
+
+		// Botão de enviar
+		loginButton = new JButton("Enter to chat");
+		loginButton.setBounds(130, 95, 120, 30);
+		loginButton.addActionListener(this);
+
+		// Adiciona à janela principal
+		this.add(loginButton);
+		this.add(usernameLabel);
+		this.add(usernameInput);
+
+		this.setVisible(true);
+	}
+
+	// Métodos de interação com o Usuario, interface ActionListener
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		JOptionPane.showMessageDialog(this, "Welcome " + usernameInput.getText(), "Welcome",
+				JOptionPane.INFORMATION_MESSAGE);
+	}
+
+	public String getUsername() {
+		return usernameInput.getText();
+	}
 
 	public static void main(String[] args) {
-
-		String nomeUsuario = JOptionPane.showInputDialog("Digite seu nome: ");
-		
-		JOptionPane.showMessageDialog(null, "Nome Usuario: "+nomeUsuario,"Nome eh:",JOptionPane.INFORMATION_MESSAGE);
-
+		Login teste = new Login();
 	}
 }
